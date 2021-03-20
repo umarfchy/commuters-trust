2b57ce2bd8f3cca8ce3cb67ba3201022f18a8291
unzippinglinux <unzippinglinux@gmail.com>
2021-03-20 19:14:19 +0600

    firebase config 1

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import './App.css';
 import Homepage from './components/Homepage/Homepage';
 import SignInForm from './components/SignInForm/SignInForm';
 import SignUpForm from './components/SignUpForm/SignUpForm';
-
+import LocationSelector from './components/LocationSelector/LocationSelector'
 
 function App() {
   return (
     <div className="App">
-      <SignUpForm></SignUpForm>
-      {/* <Homepage></Homepage> */}
+      <LocationSelector></LocationSelector>
     </div>
   );
 }
